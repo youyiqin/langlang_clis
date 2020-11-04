19d91b84e31158a84da6cdf02025803710fa334a
youyiqin <564151947@qq.com>
2020-11-04 14:22:59 +0800

    finish v1: 完成第一版,支持构建cocos/egret/course项目

--- l-work/src/commands/build.ts
+++ l-work/src/commands/build.ts
@@ -1,6 +1,8 @@
 import { Command, flags } from '@oclif/command'
 import Client, { logError, getSvnUrl } from '../lib'
 import * as Colors from 'colors'
+import cli from 'cli-ux'
+import * as inquirer from 'inquirer'
 const FindFiles = require('file-regex')
 
 export default class Build extends Command {
@@ -10,39 +12,138 @@ export default class Build extends Command {
     `$ l-work Build <path>`,
   ]
 
+  static args = [
+    { name: 'path', default: process.cwd(), description: '构建目标目录,默认为当前目录.' }
+  ]
+
   static flags = {
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    path: flags.string({ char: 'p', description: '目标路径,默认为当前路径.' }),
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f', description: '强制执行,建议不要用此标志' }),
-    level: flags.string({ char: 'l', description: '项目目录查找层级,默认查两层.因此如果当前目录下有多个需要构建的子目录,或者当前目录就是需要构建的目录,都能自动构建.' })
+    level: flags.string({ char: 'l', description: '项目目录查找层级,默认查两层.因此如果当前目录下有多个需要构建的子目录,或者当前目录就是需要构建的目录,都能自动构建.', default: '2' }),
+    env: flags.string({ char: 'e', description: '构建环境,默认使用product,可选的有226.', default: 'product', options: ['product', '226'] }),
+    template: flags.boolean({ char: 't', description: '是否使用公共模板,默认使用公共模板.', default: true })
   }
 
   async run() {
     const { args, flags } = this.parse(Build)
-
-    const targetPath = flags.path ?? process.cwd()
-    console.log(targetPath);
-
+    const targetPath = args.path
+    const level = flags.level
+    const env = flags.env
+    const template = flags.template
+    // const force = flags.force
     // get target paths
-    const result = await FindFiles(targetPath, /course\.(json|conf)$/, 2)
-    console.log(result);
-    process.exit()
-    console.log('build running.');
-    // check conf
-    Client.post('http://course.suboy.cn/cgi/auth/build/course/conf')
-      .then(res => {
-        if (res.data.code !== 0) {
-          console.log(Colors.red(res.data.message));
-        } else {
-          console.log(res.data);
+    // 一个数组,每个元素都是一个对象,包含文件名和目录名
+    cli.action.start(Colors.white('识别指定/默认地址内的目录...'))
+    let result: { dir: string, file: string }[] = await FindFiles(targetPath, /course\.(json|conf)$/, level)
+    result = result.filter(item => !item.dir.endsWith('static'))
+    cli.action.stop()
+    let buildArr: string[]
+    let buildTarget: any
+    // 如果只有一个目标,则不用选择
+    if (result.length > 1) {
+      // 选择需要构建的目标
+      let isBuildAll: any = await inquirer.prompt([{
+        name: 'all',
+        message: '是否构建此目录下所有内容?',
+        type: 'list',
+        choices: [{ name: '全部' }, { name: '由我选择' }]
+      }])
+      if (isBuildAll.all === '由我选择') {
+        buildTarget = await inquirer.prompt([
+          {
+            name: 'target',
+            message: '选择需要构建的目标:',
+            type: 'checkbox',
+            choices: result.map(item => {
+              return {
+                name: item.dir
+              }
+            })
+          }
+        ])
+      }
+      // buildTarget.target is array
+      buildArr = buildTarget.target
+    } else {
+      buildArr = result.map(item => item.dir)
+    }
+    // console.log(Colors.green('build running.'));
+    cli.action.start(Colors.white('开始构建目标...'))
+    const buildTasksArr: any[] = []
+    buildArr.forEach(async (item, index) => {
+      buildTasksArr.push(new Promise(async (resolve, reject) => {
+        // generator svn info data
+        // item is target full path, it is a directory
+        const svnData = await getSvnUrl(item)
+        const postData = {
+          env,
+          template,
+          url: svnData.url
         }
+        // check conf
+        Client.post(`http://course.suboy.cn/cgi/auth/build/${svnData.buildType}/conf`, postData)
+          .then(async res => {
+            if (res.data.code !== 0) {
+              console.log(Colors.red(res.data.message));
+            } else {
+              // 别太快,如果是构建多个目标,间歇性等待一秒钟
+              if ((index + 1) % 3 === 0) {
+                await cli.wait(1000)
+              }
+              // 配置检查没问题可以构建就构建,存在构建的目标不是想要的目标的可能,但是这个只能怪用的人...
+              Client.post(`http://course.suboy.cn/cgi/auth/build/${svnData.buildType}/start`, svnData.buildType === "course" ? postData : {
+                env,
+                url: svnData.url
+              }).then(buildRes => {
+                if (buildRes.data.code === 0) {
+                  // build successfully
+                  return resolve({
+                    status: true,
+                    dir: svnData.buildType === "course" ? res.data.data.title : res.data.data.course
+                  })
+                } else {
+                  // build failed
+                  return resolve({
+                    status: false,
+                    dir: svnData.buildType === "course" ? res.data.title : res.data.course
+                  })
+                }
+              }).catch(error => {
+                // build failed
+                logError(error.message)
+                return resolve({
+                  status: false,
+                  dir: svnData.buildType === "course" ? res.data.title : res.data.course
+                })
+              })
+            }
+          })
+          .catch(err => {
+            logError(err.message);
+            console.log(Colors.red(err.message));
+            resolve({
+              status: false,
+              dir: svnData.url.slice(svnData.url.lastIndexOf('/') + 1)
+            })
+          })
+      }))
+    })
+    // get promise all
+    Promise.all(buildTasksArr)
+      .then(result => {
+        cli.action.stop()
+        cli.table(result, {
+          target: {
+            minWidth: 48,
+            get: row => row.dir
+          },
+          status: {
+            get: row => row.status ? Colors.green('✔') : Colors.red('❌'),
+          }
+        })
       })
       .catch(err => {
-        logError(err.message);
-        console.log(Colors.red(err.message));
-        process.exit()
+        console.log(err.message);
+        logError(err.message)
       })
   }
 }
--- l-work/src/lib/index.ts
+++ l-work/src/lib/index.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import axios from 'axios'
-import svnCommands from 'node-svn-ultimate'
+const svnCommands = require('node-svn-ultimate')
 import * as Colors from 'colors'
 import { svnUrlDataType } from './types'
 
@@ -59,44 +59,48 @@ const logError = (logStr: string) => {
   }
 }
 
-const getSvnUrl = (path = process.cwd()): svnUrlDataType => {
-  svnCommands.commands.info(path, (err: any, data: any) => {
-    if (err) {
-      logError(err.message ?? err)
-      console.log(Colors.red(`无法获取 ${path} 的svn仓库信息.`));
-      process.exit()
-    }
-    const svnUrlData: svnUrlDataType = {
-      url: data.entry.url,
-      relativeUrl: data.entry['relative-url'],
-      buildType: undefined
-    }
-
-    const buildTypeArr = [
-      {
-        patternString: '^/egret_game',
-        type: 'egret'
-      },
-      {
-        patternString: '^/langlang_course',
-        type: 'course'
-      },
-      {
-        patternString: '^/cocos_game',
-        type: 'cocos'
+const getSvnUrl = async (path = process.cwd()): Promise<svnUrlDataType> => {
+  return new Promise((resolve, reject) => {
+    svnCommands.commands.info(path, (err: any, data: any) => {
+      if (err) {
+        logError(err.message ?? err)
+        console.log(Colors.red(`无法获取 ${path} 的svn仓库信息.`));
+        process.exit()
       }
-    ]
-    const isValid = buildTypeArr.some((buildTypeDict) => {
-      if (svnUrlData.relativeUrl.startsWith(buildTypeDict.patternString)) {
-        svnUrlData.buildType = buildTypeDict.type
-        return true
+      const svnUrlData: svnUrlDataType = {
+        url: data.entry.url,
+        relativeUrl: data.entry['relative-url'],
+        buildType: undefined
       }
-      return false
+      const buildTypeArr = [
+        {
+          patternString: '^/egret_game',
+          type: 'egret'
+        },
+        {
+          patternString: '^/langlang_course',
+          type: 'course'
+        },
+        {
+          patternString: '^/cocos_game',
+          type: 'cocos'
+        }
+      ]
+      const isValid = buildTypeArr.some((buildTypeDict) => {
+        if (svnUrlData.relativeUrl.startsWith(buildTypeDict.patternString)) {
+          svnUrlData.buildType = buildTypeDict.type
+          return true
+        }
+        return false
+      })
+      if (isValid) {
+        resolve(svnUrlData)
+      } else {
+        logError('此路径不是有效的可构建路径,目前仅仅支持构建课件/egret game/cocos game.')
+        console.log(Colors.red(('此路径不是有效的可构建路径,目前仅仅支持构建课件/egret game/cocos game.')));
+        process.exit()
+      }
     })
-    if (isValid) return svnUrlData
-    logError('此路径不是有效的可构建路径,目前仅仅支持构建课件/egret game/cocos game.')
-    console.log(Colors.red(('此路径不是有效的可构建路径,目前仅仅支持构建课件/egret game/cocos game.')));
-    process.exit()
   })
 }
 
