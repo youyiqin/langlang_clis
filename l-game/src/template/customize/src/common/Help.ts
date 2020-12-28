e61accbfdb2471a464a5aab71268b70371281565
youyiqin <564151947@qq.com>
2020-12-28 10:17:40 +0800

    add: some function

--- l-game/src/template/customize/src/common/Help.ts
+++ l-game/src/template/customize/src/common/Help.ts
@@ -1,6 +1,8 @@
 class Help {
-  static pipe = (...fns) => v => fns.reduce((acc, fn) => fn(acc), v);
+  // 管道,从左到右执行.只传一个参数,执行第一个函数,结果传入后续的函数.
+  static pipe = (...fns) => <T>(T) => fns.reduce((acc, fn) => fn(acc), T);
 
+  // 柯里化 将参数减少到一个
   static curry = fn => function curryFn(...args) {
     if (args.length < fn) {
       return function () {
@@ -9,22 +11,81 @@ class Help {
     }
     return fn.apply(null, args)
   }
+  // 偏函数,用undefined替代暂时不添加的函数参数,后续添加
+  static partial = (fn: Function, ...partialArgs) => {
+    let args = Array.of(partialArgs)
+    return (...fullArgs) => {
+      let argIndex = 0;
+      for (let index = 0; index < args.length && argIndex < fullArgs.length; index++) {
+        if (args[index] === undefined) {
+          args[index] = fullArgs[argIndex++]
+        }
+      }
+      const result = fn.apply(null, args)
+      args = Array.of(...partialArgs)
+      return result
+    }
+  }
+  // 方便调用egretGameApi
   static customizeEgretApi(api: 'report' | 'openVideo', argu: any): void {
     EgretGameApi && EgretGameApi[api] && EgretGameApi[api](argu)
   }
+  // 反转图片或者帧动画的touchEnabled属性
   static reverseTargetTouchEnabled(target: eui.Image | egret.MovieClip, callbackFn?: () => void) {
     try {
       target.touchEnabled = !target.touchEnabled
       callbackFn && callbackFn()
     } catch (error) {
       throw Error(error.message || `转换${target}属性失败.`)
+    } finally {
+      return target
+    }
+  }
+  // 缩放图片或者帧动画,提供一个默认实现,也可以传参数进行自定义
+  static zoomIt(target: eui.Image | egret.MovieClip, attr?: {
+    size: number
+    time: number,
+    style: Function,
+  }, cb?: Function) {
+    if (attr !== undefined) {
+      const { size, time, style } = attr
+      return egret.Tween
+        .get(target)
+        .to({
+          scaleX: size,
+          scaleY: size
+        }, time, style)
+        .call(() => {
+          cb && cb()
+        })
+    } else {
+      // 默认缩放
+      return egret.Tween
+        .get(target)
+        .to({
+          scaleX: 1.2,
+          scaleY: 1.2
+        }, 800, egret.Ease.sineInOut)
+        .to({
+          scaleX: 1,
+          scaleY: 1
+        }, 800, egret.Ease.sineIn)
+        .call(() => {
+          cb && cb()
+        })
     }
   }
 
+  // 设置图片或者帧动画的锚点到物体的中心,方便原地缩放和旋转等等操作.
   static setCenterAnchorOffset(target: eui.Image | egret.MovieClip | egret.Shape) {
     target.anchorOffsetX = target.width / 2;
     target.x += target.width / 2;
     target.anchorOffsetY = target.height / 2;
     target.y += target.height / 2;
+  }
+  // 例如图片有 tu1~tu10
+  // 可以 rangeOnTarget(this, 10, 'tu', 1) 得到 [tu1, tu2....tu10] 的数组
+  static rangeOnTarget(target: any, length: number, keyword: string, from: number = 0) {
+    return Array.from({ length }, (_, i) => target[`${keyword}${i + from}`])
   }
 }
