6f2a1fc105b993b7d5304834eb62418b2d2a74d5
youyiqin <564151947@qq.com>
2020-12-29 10:16:21 +0800

    add: time command

---
+++ l-work/src/commands/time.ts
@@ -0,0 +1,21 @@
+import { Command, flags } from '@oclif/command'
+const clipboardy = require('clipboardy')
+
+export default class Time extends Command {
+  static description = '幼小识字-书写乐园,将文字的时间区间整理成数组,并且粘贴到剪贴板.'
+
+  static examples = [
+    `$ l-work time`,
+  ]
+
+  async run() {
+    // get clipborad strings
+    const targetString = clipboardy.readSync()
+    const result = targetString.split('\r\n')
+      .filter((line: string) => line.endsWith('s'))
+      .map((line: string) => line.match(/([0-9.]+)s$/)[1])
+    console.log(result);
+    clipboardy.writeSync(`[${[0, ...result]}]`)
+  }
+}
+
