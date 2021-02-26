eafeb7244f79355daecf07b8283cf15c36d334d4
youyiqin <564151947@qq.com>
2021-02-26 14:58:08 +0800

    auto update...

--- l-game/src/template/customize/src/common/Utils.ts
+++ l-game/src/template/customize/src/common/Utils.ts
@@ -4,19 +4,17 @@ class MyLodash {
      * 打乱数组并且返回新数组
      */
     static shuffle(arr: any[]) {
-        const length = arr.length;
-        if (length === 0) return []
-        let index = -1;
-        const lastIndex = length - 1;
-        const result = [...arr];
-        while (++index < length) {
-            // 拿到当前数组长度内的随机下标
-            const rand = index + Math.floor(Math.random() * (lastIndex - index + 1))
-            const value = result[rand];
-            // 互换随机元素和当前index下的元素,最终所有当前index下的元素都被替换成随机元素
-            result[rand] = result[index];
-            result[index] = value
+        let length = arr.length;
+        while (length > 1) {
+            const random = ~~(Math.random() * length)
+            let temp = arr[length]
+            arr[length] = arr[random]
+            arr[random] = temp
         }
-        return result
+        return arr
+    }
+
+    static range(size: number, start = 0) {
+        return Array.from({ length: size }, (_, i) => i + start)
     }
 }
